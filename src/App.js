7cce9c4a617cbc68d0507c62fa869f6f50aad16d
fehbs <fernandobailao@gmail.com>
2022-06-23 17:51:26 -0300

    Initialized project

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import { useEffect, useState } from 'react';
+
+import './App.css';
+
+// api: https://api.chucknorris.io/jokes/random
+const API_URL = 'https://api.chucknorris.io/jokes/random';
+
+
+function App() {
+  const [joke, setJoke] = useState('');
+  
+
+  const generateJoke = () => {
+
+    fetch(API_URL)
+    .then(res => res.json())
+    .then(data => setJoke(data.value));
+   
+  }
+
+  useEffect(() => {
+    generateJoke();
+  }, [])
+
+  return (
+    <div className="App">
+      <img src='https://cdn.dribbble.com/users/24711/screenshots/1701350/media/36a576751c06d476f6db6ac9250586d1.png?compress=1' alt='img' width='20%'></img>
+     <h1>Chuck Norris Jokes Generator</h1>
+     <p>{joke}</p>
+     
+     <button onClick={generateJoke}>Get new joke: 😻 </button>
+    </div>
+  );
+}
+
+export default App;
+
+
+
