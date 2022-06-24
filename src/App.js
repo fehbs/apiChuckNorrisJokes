61898636b519ad7fdc36e8cb3ad9fe12af1d340f
fehbs <fernandobailao@gmail.com>
2022-06-24 17:23:43 -0300

    In progress

--- src/App.js
+++ src/App.js
@@ -17,24 +17,31 @@ function App() {
     fetch(API_URL)
       .then(res => res.json())
       .then(data => setJoke(data.value));
+     
 
   }
 
   useEffect(() => {
     generateJoke();
+
   }, [])
+
+  
 
   return (
     <div className="App">
+
       <div className="App-header">
         <img className='App-logo' src='https://cdn.dribbble.com/users/24711/screenshots/1701350/media/36a576751c06d476f6db6ac9250586d1.png?compress=1' alt='img' width='20%'></img>
         <h1>Chuck Norris Jokes Generator</h1>
-        <p>{joke}</p>
-     
 
         <button onClick={generateJoke}>Get new joke: 😻 </button>
-        
+        <div className='App-p'>
+          <p>{joke}</p>
+        </div>
+
       </div>
+
     </div>
   );
 }
