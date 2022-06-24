f665dbc60654523ea091bde1721723c66cdf4797
fehbs <fernandobailao@gmail.com>
2022-06-24 14:05:24 -0300

    Material Ui Lib installed

--- src/App.js
+++ src/App.js
@@ -2,20 +2,25 @@ import { useEffect, useState } from 'react';
 
 import './App.css';
 
+import * as React from 'react';
+import ReactDOM from 'react-dom';
+import Button from '@mui/material/Button';
+
+
 // api: https://api.chucknorris.io/jokes/random
 const API_URL = 'https://api.chucknorris.io/jokes/random';
 
 
 function App() {
   const [joke, setJoke] = useState('');
-  
+
 
   const generateJoke = () => {
 
     fetch(API_URL)
-    .then(res => res.json())
-    .then(data => setJoke(data.value));
-   
+      .then(res => res.json())
+      .then(data => setJoke(data.value));
+
   }
 
   useEffect(() => {
@@ -24,11 +29,13 @@ function App() {
 
   return (
     <div className="App">
-      <img src='https://cdn.dribbble.com/users/24711/screenshots/1701350/media/36a576751c06d476f6db6ac9250586d1.png?compress=1' alt='img' width='20%'></img>
-     <h1>Chuck Norris Jokes Generator</h1>
-     <p>{joke}</p>
-     
-     <button onClick={generateJoke}>Get new joke: 😻 </button>
+      <div className="App-header">
+        <img className='App-logo' src='https://cdn.dribbble.com/users/24711/screenshots/1701350/media/36a576751c06d476f6db6ac9250586d1.png?compress=1' alt='img' width='20%'></img>
+        <h1>Chuck Norris Jokes Generator</h1>
+        <p>{joke}</p>
+        <button onClick={generateJoke}>Get new joke: 😻 </button>
+        <Button variant="contained"> Olá Mundo</Button>
+      </div>
     </div>
   );
 }
