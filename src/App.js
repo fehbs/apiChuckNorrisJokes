3bebdada1fb0c458ccef31622912dfce4ec06d9d
fehbs <fernandobailao@gmail.com>
2022-06-29 12:48:24 -0300

    In porgress

--- src/App.js
+++ src/App.js
@@ -7,26 +7,20 @@ import './App.css';
 // api: https://api.chucknorris.io/jokes/random
 const API_URL = 'https://api.chucknorris.io/jokes/random';
 
-
 function App() {
   const [joke, setJoke] = useState('');
-
 
   const generateJoke = () => {
 
     fetch(API_URL)
       .then(res => res.json())
-      .then(data => setJoke(data.value));
-     
-
+      .then(data => setJoke(data.value));  
   }
 
   useEffect(() => {
     generateJoke();
 
-  }, [])
-
-  
+  }, []);
 
   return (
     <div className="App">
@@ -35,9 +29,9 @@ function App() {
         <img className='App-logo' src='https://cdn.dribbble.com/users/24711/screenshots/1701350/media/36a576751c06d476f6db6ac9250586d1.png?compress=1' alt='img' width='20%'></img>
         <h1>Chuck Norris Jokes Generator</h1>
 
-        <button onClick={generateJoke}>Get new joke: 😻 </button>
+        <button onClick={generateJoke}>Get new joke:😈 </button>
         <div className='App-p'>
-          <p>{joke}</p>
+          <p> {joke} </p>
         </div>
 
       </div>
